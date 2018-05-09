271a11ab61dea090519f24d00bf2b9b6a186dfdd
James-Duan <18910329670@163.com>
2018-05-09 20:27:09 +0800

    添加add方法

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import os
 
 
+def add(a, b):
+    return a + b
+
+
 def get_path():
     return os.getcwd()
 
 
 def main():
     print "path %s" % get_path()
+    print "2+3=%s" % add(2, 3)
 
 
 if __name__ == '__main__':
